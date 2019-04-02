c0bce88fe8f661fd529f35c5762df3e5581ff1f6
Anton Gulenko <anton.gulenko@googlemail.com>
2019-04-02 17:05:03 +0200

    Revert "Testing mkdocs"
    This reverts commit f70f6c9850ba528052327180e3e4e2f0d92915c5.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,41 +40,13 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-# -- Readthedocs specific stuff ----------------------------------------------
-# This is used for linking and such so we link to the thing we're building
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-rtd_version = os.environ.get('READTHEDOCS_VERSION', 'latest')
-if rtd_version not in ['stable', 'latest']:
-    rtd_version = 'stable'
-
-subprojects = [ 'python-bitflow', 'go-bitflow', 'go-bitflow-collector', 'bitflow-antlr-grammars' ]
-intersphinx_mapping = { subproject: ('http://%s.readthedocs.io/en/%s/' % (subproject, rtd_version), None) for subproject in subprojects }
-
 master_doc = 'index'
-
 # -- Options for HTML output -------------------------------------------------
 source_suffix = {
     '.rst': 'restructuredtext',
     '.txt': 'markdown',
     '.md': 'markdown',
 }
-
-html_theme_options = {
-    #'canonical_url': '',
-    #'logo_only': False,
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'vcs_pageview_mode': '',
-    'style_nav_header_background': 'white',
-    # Toc options
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False
-}
-
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
@@ -83,4 +55,3 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
